bd24f0ebff3b85ececff18a7efca0654a95c94e7
Anurag kumar <anuragkmr825@gmail.com>
2023-09-26 07:29:53 +0530

    .

--- src/components/badge/index.jsx
+++ src/components/badge/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Badge from 'react-bootstrap/Badge';
 
 const BadgeComponent = ({ pageCounter, toggleSidebar, sidebarVisible  }) => {
