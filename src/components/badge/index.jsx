b2cdf5a67a429a6b654215ed45640b487edcb57b
anuragkmr45 <anuragkmr825@gmail.com>
2023-10-16 06:40:34 +0530

    /.

--- src/components/badge/index.jsx
+++ src/components/badge/index.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import Badge from 'react-bootstrap/Badge';
 
-const BadgeComponent = ({ pageCounter, toggleSidebar, sidebarVisible  }) => {
+const BadgeComponent = ({ pageCounter, toggleSidebar, sidebarVisible }) => {
 
     return (
         <>
-        <div onClick={toggleSidebar}>
             <h4>
                 <Badge bg="light" text="dark" className="px-lg-3">
                     {pageCounter} /3
                 </Badge>
             </h4>
-        </div>
-
-        
-      {/* Optional: Display a message based on sidebar visibility */}
-      {sidebarVisible && <p>Sidebar is visible</p>}
         </>
     );
 }
--- src/pages/edit-deck/index.jsx
+++ src/pages/edit-deck/index.jsx
@@ -25,81 +25,81 @@ firebase.initializeApp(firebaseConfig);
 
 
 const EditDeck = () => {
-    const [pageCounter, setPageCounter] = useState(1);
+  const [pageCounter, setPageCounter] = useState(1);
 
-    const bgImg = "https://rentblob.blob.core.windows.net/pitch/img/Pitch-Catalyst/home-page-imgs/vector53.png";
+  const bgImg = "https://rentblob.blob.core.windows.net/pitch/img/Pitch-Catalyst/home-page-imgs/vector53.png";
 
-    const breadcrumbItems = [
-        { text: 'Home', url: '/' },
-        { text: 'Edit Deck' },
-    ];
+  const breadcrumbItems = [
+    { text: 'Home', url: '/' },
+    { text: 'Edit Deck' },
+  ];
 
-    const [sidebarVisible, setSidebarVisible] = useState(false);
-    const sidebarRef = useRef(null);
-  
-    const toggleSidebar = () => {
-      setSidebarVisible(!sidebarVisible);
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const sidebarRef = useRef(null);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
+
+  useEffect(() => {
+    if (sidebarVisible) {
+      document.body.style.overflow = 'hidden';
+      document.addEventListener('click', handleOutsideClick);
+    } else {
+      document.body.style.overflow = 'unset';
+      document.removeEventListener('click', handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
     };
-  
-    useEffect(() => {
-      if (sidebarVisible) {
-        document.body.style.overflow = 'hidden';
-        document.addEventListener('click', handleOutsideClick);
-      } else {
-        document.body.style.overflow = 'unset';
-        document.removeEventListener('click', handleOutsideClick);
-      }
-  
-      return () => {
-        document.removeEventListener('click', handleOutsideClick);
-      };
-    }, [sidebarVisible]);
-  
-    const handleOutsideClick = (event) => {
-      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
-        setSidebarVisible(false);
-      }
+  }, [sidebarVisible]);
+
+  const handleOutsideClick = (event) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+      setSidebarVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'unset';
     };
-
-    useEffect(() => {
-        document.body.style.overflow = 'hidden';
-        return () => {
-            document.body.style.overflow = 'unset';
-        };
-    }, []);
+  }, []);
 
 
-    return (
-        <div style={{ backgroundColor: '#060606', backgroundImage: `url(${bgImg})`, backgroundSize: 'cover', height: '85vh', overflowY: 'none' }} >
-            <div className="d-flex justify-content-between p-1">
-                <div className="d-flex flex-column">
-                    <Breadcrumb items={breadcrumbItems} />
-                    <div className="d-flex">
-                        <AiOutlineLeft style={{ fontSize: '2rem' }} />
-                        <div className="d-flex flex-column mx-2">
-                            <h5 className='my-0' >
-                                Edit Your <br />
-                                <span style={{ color: '#E1FF00' }} >Pitch</span> Deck
-                            </h5>
-                            <hr style={{
-                                border: '1px solid #E1FF00',
-                                width: '8rem'
-                            }} />
-                        </div>
-                    </div>
-                    <SideNav />
-                </div>
-                <div className="d-flex mt-1 ">
-                    <p className='mx-3 mt-1' >Total <br /> <span style={{ color: '#E1FF00' }}>Slides</span></p>
-                    <Badge 
-                    pageCounter={pageCounter} 
-                    toggleSidebar={toggleSidebar}
-                    sidebarVisible={sidebarVisible}
-                    />
-                </div>
+  return (
+    <div style={{ backgroundColor: '#060606', backgroundImage: `url(${bgImg})`, backgroundSize: 'cover', height: '85vh', overflowY: 'none' }} >
+      <div className="d-flex justify-content-between p-1">
+        <div className="d-flex flex-column">
+          <Breadcrumb items={breadcrumbItems} />
+          <div className="d-flex">
+            <AiOutlineLeft style={{ fontSize: '2rem' }} />
+            <div className="d-flex flex-column mx-2">
+              <h5 className='my-0' >
+                Edit Your <br />
+                <span style={{ color: '#E1FF00' }} >Pitch</span> Deck
+              </h5>
+              <hr style={{
+                border: '1px solid #E1FF00',
+                width: '8rem'
+              }} />
             </div>
+          </div>
+          <SideNav />
+        </div>
+        <div className="d-flex mt-1 ">
+          <p className='mx-3 mt-1' >Total <br /> <span style={{ color: '#E1FF00' }}>Slides</span></p>
+          <Badge
+            pageCounter={pageCounter}
+            toggleSidebar={toggleSidebar}
+            sidebarVisible={sidebarVisible}
+          />
+        </div>
+      </div>
 
-            {sidebarVisible && (
+      {sidebarVisible && (
         <div
           ref={sidebarRef}
           style={{
@@ -117,32 +117,32 @@ const EditDeck = () => {
         </div>
       )}
 
-            <div className="d-flex justify-content-center">
-                <div
-                    style={{
-                        width: '15%',
-                        height: '200vh',
-                        backgroundColor: 'white',
-                        position: 'absolute',
-                        top: '12%',
-                    }} ></div>
-            </div>
+      <div className="d-flex justify-content-center">
+        <div
+          style={{
+            width: '15%',
+            height: '200vh',
+            backgroundColor: 'white',
+            position: 'absolute',
+            top: '12%',
+          }} ></div>
+      </div>
 
-            <div
-                className='text-center'
-                style={{
-                    zIndex: '10',
-                    position: 'absolute',
-                    bottom: '0',
-                    width: '100%',
-                }}>
-                <EditModal pageCounter={pageCounter} />
-                <CarouselComp pageCounter={pageCounter} setPageCounter={setPageCounter} />
+      <div
+        className='text-center'
+        style={{
+          zIndex: '10',
+          position: 'absolute',
+          bottom: '0',
+          width: '100%',
+        }}>
+        <EditModal pageCounter={pageCounter} />
+        <CarouselComp pageCounter={pageCounter} setPageCounter={setPageCounter} />
 
-            </div>
-            <ToastContainer position="bottom-center" />
-        </div>
-    )
+      </div>
+      <ToastContainer position="bottom-center" />
+    </div>
+  )
 }
 
 export default EditDeck
